97d6a26f4809eb0922d55e2c3556217a408b47dc
Azfar Razzaq <azfarrazzaq23@gmail.com>
2025-07-23 17:29:57 +0500

    added hover functionality

--- src/Carousel.tsx
+++ src/Carousel.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css"; // adjust if needed
 interface BaseCarouselProps {
   className?: string;
   num_of_copies?: number;
   animationDuration?: number;
   animationDirection?: "ltr" | "rtl";
+  hoverSpeedFactor?: number;
 }
 
 // For image carousels
@@ -22,14 +23,31 @@ interface TextCarouselProps extends BaseCarouselProps {
 // Discriminated union type
 type CarouselProps = ImageCarouselProps | TextCarouselProps;
 const Carousel: React.FC<CarouselProps> = (params: CarouselProps) => {
+  const [hovered, setHovered] = useState(false);
+
+  const effectiveDuration = getEffectiveDuration({
+    hovered,
+    hoverSpeedFactor: params.hoverSpeedFactor,
+    baseDuration: params.animationDuration || 20,
+  });
+
   const numCopies = params.num_of_copies || 2;
+
   const animationStyle = {
-    animation: `marquee ${params.animationDuration ? `${params.animationDuration.toString()}s` : "20s"} linear infinite ${params.animationDirection === "rtl" ? "reverse" : ""}`,
+    animation: `marquee ${effectiveDuration}s linear infinite   ${
+      params.animationDirection === "rtl" ? "reverse" : ""
+    }`,
   };
 
   if (params.itemType === "images") {
     const carouselCopies = Array.from({ length: numCopies }, (_, i) => (
-      <div key={`marquee-${i}`} className="marquee" style={animationStyle}>
+      <div
+        key={`marquee-${i}`}
+        className="marquee"
+        style={animationStyle}
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+      >
         {params.images_links.map((link, index) => (
           <img
             key={`img-${i}-${index}`}
@@ -53,12 +71,21 @@ const Carousel: React.FC<CarouselProps> = (params: CarouselProps) => {
 
 export default Carousel;
 
-//extra code:
-
-// {
-//   <div className="marquee">
-//     {params.images_links.map((link, index) => (
-//       <img key={`A${index}`} src={link} className alt="Carousel Item" />
-//     ))}
-//   </div>;
-// }
+function getEffectiveDuration({
+  hovered,
+  hoverSpeedFactor,
+  baseDuration,
+}: {
+  hovered: boolean;
+  hoverSpeedFactor: number | undefined;
+  baseDuration: number;
+}) {
+  if (hovered && hoverSpeedFactor !== undefined && hoverSpeedFactor !== 1) {
+    if (hoverSpeedFactor === 0) {
+      return 100000; // simulate pause
+    } else {
+      return baseDuration / hoverSpeedFactor;
+    }
+  }
+  return baseDuration;
+}
